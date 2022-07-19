74f7dfde29bbf5a82932870fecdc37d08007c408
unknown <phihung260701@gmai.com>
2022-07-19 23:30:54 +0700

    case

--- case_study/sql_co_ban_1_5.sql
+++ case_study/sql_co_ban_1_5.sql
@@ -40,9 +40,36 @@ FROM
 WHERE
     lk.ten_loai_khach = 'Diamond'
 GROUP BY ma_khach_hang
-ORDER BY COUNT(kh.ma_khach_hang)
+ORDER BY COUNT(kh.ma_khach_hang);
 
-
+-- 5.Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien 
+-- (Với tổng tiền được tính theo công thức như sau: 
+-- Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) 
+-- cho tất cả các khách hàng đã từng đặt phòng. 
+-- (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+    
+SELECT 
+	kh.ma_khach_hang,
+    kh.ho_ten,
+    lk.ten_loai_khach,
+    hd.ma_hop_dong,
+    dv.ten_dich_vu,
+    hd.ngay_lam_hop_dong,
+    hd.ngay_ket_thuc,
+    (dv.chi_phi_thue + dvdk.gia * hdct.so_luong) AS 'tong_tien'
+FROM
+    hop_dong hd
+        JOIN
+    dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu
+       JOIN
+    hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+      JOIN
+    dich_vu_di_kem dvdk ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+	 RIGHT JOIN
+    khach_hang kh ON kh.ma_khach_hang = hd.ma_khach_hang
+      JOIN
+    loai_khach lk ON lk.ma_loai_khach = kh.ma_loai_khach
+ORDER BY(kh.ma_khach_hang);
 
  
 
